96cc382499dc7ad5da74b29b7ce5df51249fe9ae
kobat@kobat.dev <kobat@kobat.dev>
2021-04-25 19:56:02 +0900

    プロジェクト ファイルを追加します。

---
+++ AntiModernStandby/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace AntiModernStandby
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButtonNone = new System.Windows.Forms.RadioButton();
+            this.radioButtonSystemRequired = new System.Windows.Forms.RadioButton();
+            this.radioButtonDisplayRequired = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // radioButtonNone
+            // 
+            this.radioButtonNone.AutoSize = true;
+            this.radioButtonNone.Checked = true;
+            this.radioButtonNone.Location = new System.Drawing.Point(12, 12);
+            this.radioButtonNone.Name = "radioButtonNone";
+            this.radioButtonNone.Size = new System.Drawing.Size(80, 29);
+            this.radioButtonNone.TabIndex = 2;
+            this.radioButtonNone.TabStop = true;
+            this.radioButtonNone.Text = "None";
+            this.radioButtonNone.UseVisualStyleBackColor = true;
+            this.radioButtonNone.CheckedChanged += new System.EventHandler(this.radioButtonNone_CheckedChanged);
+            // 
+            // radioButtonSystemRequired
+            // 
+            this.radioButtonSystemRequired.AutoSize = true;
+            this.radioButtonSystemRequired.Location = new System.Drawing.Point(12, 47);
+            this.radioButtonSystemRequired.Name = "radioButtonSystemRequired";
+            this.radioButtonSystemRequired.Size = new System.Drawing.Size(192, 29);
+            this.radioButtonSystemRequired.TabIndex = 3;
+            this.radioButtonSystemRequired.Text = "SYSTEM_REQUIRED";
+            this.radioButtonSystemRequired.UseVisualStyleBackColor = true;
+            this.radioButtonSystemRequired.CheckedChanged += new System.EventHandler(this.radioButtonSystemRequired_CheckedChanged);
+            // 
+            // radioButtonDisplayRequired
+            // 
+            this.radioButtonDisplayRequired.AutoSize = true;
+            this.radioButtonDisplayRequired.Location = new System.Drawing.Point(12, 82);
+            this.radioButtonDisplayRequired.Name = "radioButtonDisplayRequired";
+            this.radioButtonDisplayRequired.Size = new System.Drawing.Size(195, 29);
+            this.radioButtonDisplayRequired.TabIndex = 4;
+            this.radioButtonDisplayRequired.Text = "DISPLAY_REQUIRED";
+            this.radioButtonDisplayRequired.UseVisualStyleBackColor = true;
+            this.radioButtonDisplayRequired.CheckedChanged += new System.EventHandler(this.radioButtonDisplayRequired_CheckedChanged);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 139);
+            this.Controls.Add(this.radioButtonDisplayRequired);
+            this.Controls.Add(this.radioButtonNone);
+            this.Controls.Add(this.radioButtonSystemRequired);
+            this.Name = "FormMain";
+            this.Text = "AntiModernStandby";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormMain_FormClosed);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RadioButton radioButtonNone;
+        private System.Windows.Forms.RadioButton radioButtonSystemRequired;
+        private System.Windows.Forms.RadioButton radioButtonDisplayRequired;
+    }
+}
+
---
+++ AntiModernStandby/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace AntiModernStandby
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private Timer timer;
+        private StreamWriter writer;
+        
+        private void log(Object o)
+        {
+            System.Diagnostics.Debug.WriteLine(o);
+            writer.WriteLine(o);
+            writer.Flush();
+        }
+
+        // SetThreadExecutionState
+        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        static extern uint SetThreadExecutionState(uint esFlags);
+        private const uint ES_AWAYMODE_REQUIRED = 0x00000040;
+        private const uint ES_CONTINUOUS = 0x80000000;
+        private const uint ES_DISPLAY_REQUIRED = 0x00000002;
+        private const uint ES_SYSTEM_REQUIRED = 0x00000001;
+        private const uint ES_USER_PRESENT = 0x00000004;
+
+        // SC_MONITORPOWER
+        // -1 (the display is powering on)
+        //  1 (the display is going to low power)
+        //  2 (the display is being shut off)
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern IntPtr SendMessage(int hWnd, uint Msg, int wParam, int lParam);
+        private const uint WM_SYSCOMMAND = 0x0112;
+        private const int SC_MONITORPOWER = 0xF170;
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            timer = new Timer();
+            timer.Interval = 30 * 1000;
+            timer.Tick += new EventHandler((ss, ee) => {
+                var dt = DateTime.Now;
+                //System.Diagnostics.Debug.WriteLine(dt);
+                //writer.WriteLine(dt);
+                log(dt);
+            });
+
+            writer = new StreamWriter(Application.StartupPath + "\\AntiModernStandby.log");
+            log("-- START");
+        }
+
+        private void FormMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            log("-- CLOSE");
+            writer.Close();
+        }
+        
+        private void radioButtonNone_CheckedChanged(object sender, EventArgs e)
+        {
+            uint result = SetThreadExecutionState(ES_CONTINUOUS);
+            timer.Enabled = false;
+
+            log("-- NONE");
+        }
+
+        private void radioButtonSystemRequired_CheckedChanged(object sender, EventArgs e)
+        {
+            log("-- ES_SYSTEM_REQUIRED");
+
+            uint result = SetThreadExecutionState(ES_SYSTEM_REQUIRED | ES_CONTINUOUS);
+            timer.Enabled = true;
+        }
+
+        private void radioButtonDisplayRequired_CheckedChanged(object sender, EventArgs e)
+        {
+            log("-- ES_DISPLAY_REQUIRED");
+
+            uint result = SetThreadExecutionState(ES_DISPLAY_REQUIRED | ES_CONTINUOUS);
+            timer.Enabled = true;
+        }
+
+        //private void buttonMonitorPowerOff_Click(object sender, EventArgs e)
+        //{
+        //    Thread.Sleep(1000); // Wait a little after clicking this button.
+        //    SendMessage(-1, WM_SYSCOMMAND, SC_MONITORPOWER, 2);
+        //}
+    }
+}
